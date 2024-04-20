a641bc7e6a112c37bd5e0a4c2a33585ea9d16b57
RyanWakey <wake52@outlook.com>
2024-04-20 06:24:36 +0100

    Added routes to homepage and product details

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
+import ProductDetails from './components/ProductDetails'; // You will need to create this component
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/product/:id" component={ProductDetails} />
+        {}
+      </Switch>
+    </Router>
   );
 }
 
