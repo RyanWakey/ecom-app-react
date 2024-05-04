e28595a9da0501bbc9276a33371e57e830c51ea2
RyanWakey <wake52@outlook.com>
2024-05-04 06:37:38 +0100

    Products corectly displayed on hompeage

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,10 +7,11 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  // Assume images is an array of objects, and we are accessing the 'url' property
   const imageUrl = product.images.length > 0
     ? (product.images[0].url.startsWith('http') ? product.images[0].url : `${process.env.REACT_APP_API_BASE_URL}${product.images[0].url}`)
-    : '/path/to/default/image/Emazon.png';  // Provide a default image path if no images exist
+    : '/path/to/default/image/Emazon.png';
+
+  const price = parseFloat(product.price.toString());
 
   return (
     <div className="border-2 border-gray-300 p-4 flex flex-col items-center justify-between h-full">
@@ -21,7 +22,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
         <Link to={`/product/${product.id}`}>
           <h2 className="text-lg font-bold mb-1">{product.name}</h2>
         </Link>
-        <p className="mb-2">${product.price.toFixed(2)}</p>
+        <p className="mb-2">${price.toFixed(2)}</p>
         <p className="mb-4 text-sm text-gray-500">{product.stock > 0 ? 'In Stock' : 'Out of Stock'}</p>
       </div>
     </div>
