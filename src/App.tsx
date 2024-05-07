5691af3d2b8c684c662ac2f79e1e0e447627ea0b
RyanWakey <wake52@outlook.com>
2024-05-07 06:49:27 +0100

    Added second nav to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import ProductDetails from './components/ProductDetails';
 import Navbar from './components/TopNavBar'; 
+import SecondaryNav from './components/SecondaryNav'; 
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
+        {/* Existing Top Navigation Bar */}
         <Navbar cartItemCount={0} />
+
+        {/* New Secondary Navigation Bar */}
+        <SecondaryNav />
+
+        {/* Main Content */}
         <div className="flex-grow">
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/product/:id" element={<ProductDetails />} />
           </Routes>
         </div>
+
+        {/* Footer */}
         <Footer />
       </div>
     </Router>
   );
 };
+
 
 export default App;
