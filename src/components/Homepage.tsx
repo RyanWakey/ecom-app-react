37afc91a2e40dc76756cc3d9ac8c119b82f05e17
RyanWakey <wake52@outlook.com>
2024-05-11 04:03:28 +0100

    Added Banner to homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import TopBannerCarousel from './TopBannerCarousel'; 
 
 const Homepage = () => {
   return (
     <div className="container mx-auto mt-4">
+      <TopBannerCarousel />
     </div>
   );
 };
