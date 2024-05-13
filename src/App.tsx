2801b5b14d8a3b7e75cdc16224ec4fb9a5a0cb9e
RyanWakey <wake52@outlook.com>
2024-05-13 05:41:32 +0100

    corrected style for width of banners

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-// App.tsx
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -3,7 +3,7 @@ import TopBannerCarousel from './TopBannerCarousel';
 
 const Homepage = () => {
   return (
-    <div className="container mx-auto mt-4">
+    <div className="flex flex-col lg:flex-row">
       <TopBannerCarousel />
     </div>
   );
