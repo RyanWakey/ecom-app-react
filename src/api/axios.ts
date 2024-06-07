99aad1f8ccc8534bb58ddf962aac881b09134c91
RyanWakey <wake52@outlook.com>
2024-06-07 03:45:37 +0100

    updated api base url for axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:80',
+  baseURL: process.env.REACT_APP_API_BASE_URL, // 'http://localhost:80'
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
